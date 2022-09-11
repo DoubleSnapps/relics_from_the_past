63f36426efb90c1acf63f7676c649f0f86f217cf
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2022-09-11 18:41:03 -0500

    Began SuperHeavyBow documentation and finished ruined tower nbt

---
+++ src/main/java/doublesnapps/relics/items/SuperHeavyBow.java
@@ -0,0 +1,21 @@
+package doublesnapps.relics.items;
+import net.minecraft.item.BowItem;
+
+public class SuperHeavyBow {
+
+    public class SuperHeavyBow extends BowItem
+        public SuperHeavyBow(Settings settings){
+        super(new BowItem(),)
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
